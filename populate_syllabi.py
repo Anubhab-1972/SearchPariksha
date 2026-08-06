36d1eabb866719fcef198165ba46cc27b141a246
Anubhab <anubhab@searchpariksha.com>
2026-08-06 23:40:59 +0530

    Fix tools syntax in syllabus fetcher

--- populate_syllabi.py
+++ populate_syllabi.py
@@ -38,7 +38,7 @@ def fetch_syllabus(exam_name, exam_desc):
             contents=prompt,
             config=types.GenerateContentConfig(
                 temperature=0.1,
-                tools=[{"google_search": {}}],
+                tools=[types.Tool(google_search=types.GoogleSearch())],
                 response_mime_type="application/json"
             )
         )
